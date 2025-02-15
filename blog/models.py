f5c5123307a10123e70212e9e17977fca9dfce75
Lileck <miroshnikovalili@mail.ru>
2025-02-15 04:46:34 +0300

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -55,3 +55,40 @@ class Post(models.Model):
         verbose_name = 'пост'
         verbose_name_plural = 'публикации'
         unique_together = ('category', 'slug')
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=255)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    parent_comment = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='replies')
+
+    def __str__(self):
+        return f"Comment by {self.name} on {self.post.title}"
+
+    class Meta:
+        verbose_name = "Комментарий"
+        verbose_name_plural = "Комментарии"
+
+class Like(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')
+    created_at = models.DateTimeField(auto_now_add=True)
+    reaction = models.IntegerField(choices=[(1, 'Лайк'), (-1, 'Дизлайк')])
+
+    def __str__(self):
+        return f"Like on {self.post.title}"
+
+    class Meta:
+        verbose_name = "Лайк"
+        verbose_name_plural = "Лайки"
+
+class Donation(models.Model):
+    title = models.CharField(max_length=255)
+    link = models.URLField()
+    image = models.ImageField(upload_to='donations/')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Донат"
+        verbose_name_plural = "Донаты"
